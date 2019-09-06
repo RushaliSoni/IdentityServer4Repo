31449e065982e8a8e919f028180ad8d6e9b321c2
akshansha <akshanshagohil1995@gmail.com>
2019-09-06 19:43:09 +0530

    Add project files.

---
+++ src/Marvin.IDP/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Marvin.IDP.Entities;
+using Marvin.IDP.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Marvin.IDP
+{
+    public class Startup
+    {
+        public static IConfiguration Configuration;
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionstrings = Configuration["connectionStrings:marvinUserDBConnectionString"];
+            services.AddDbContext<MarvinUserContext>(o => o.UseSqlServer(connectionstrings));
+
+            services.AddScoped<IMarvinUserRepository, MarvinUserRepository>();
+
+            var identityServerDataDBConnectionString = Configuration["identityServerDataDBConnectionString"];
+
+            var migrationsAssembly = typeof(Startup)
+                .GetTypeInfo().Assembly.GetName().Name;
+            services.AddMvc();
+
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddMarvinUserStore()
+                .AddConfigurationStore(builder =>
+                    builder.UseSqlServer(identityServerDataDBConnectionString,
+                    options => options.MigrationsAssembly(migrationsAssembly)));
+                //.AddOperationalStore(builder =>
+                //    builder.UseSqlServer(identityServerDataDBConnectionString,
+                //    options => options.MigrationsAssembly(migrationsAssembly)));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+           // loggerFactory.AddConsole();
+           // loggerFactory.AddDebug();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //marvinUserContext.Database.Migrate();
+            //marvinUserContext.EnsureSeedDataForContext();
+
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
